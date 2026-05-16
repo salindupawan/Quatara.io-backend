985cc7dd383712947816f1debfc924927eb0cb95
salindupawan <salindupa@gmail.com>
2026-05-16 17:45:33 +0530

    feat: rate limiting funtinality

--- src/main/java/io/quatara/backend/config/SecurityConfig.java
+++ src/main/java/io/quatara/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.quatara.backend.config;
 import io.quatara.backend.security.ClerkAccessDeniedHandler;
 import io.quatara.backend.security.ClerkAuthenticationEntryPoint;
 import io.quatara.backend.security.ClerkJwtAuthenticationConverter;
+import io.quatara.backend.security.RateLimitingFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,15 +21,18 @@ public class SecurityConfig {
     private final ClerkJwtAuthenticationConverter clerkJwtAuthenticationConverter;
     private final ClerkAuthenticationEntryPoint clerkAuthenticationEntryPoint;
     private final ClerkAccessDeniedHandler clerkAccessDeniedHandler;
+    private final RateLimitingFilter rateLimitingFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(req ->
-                req.anyRequest().authenticated()
-        ).oauth2ResourceServer(auth2 -> auth2
-                .jwt(jwt -> jwt.jwtAuthenticationConverter(clerkJwtAuthenticationConverter))
-                .accessDeniedHandler(clerkAccessDeniedHandler)
-                .authenticationEntryPoint(clerkAuthenticationEntryPoint)
+        http
+                .addFilterBefore(rateLimitingFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(req -> req
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(auth2 -> auth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(clerkJwtAuthenticationConverter))
+                        .accessDeniedHandler(clerkAccessDeniedHandler)
+                        .authenticationEntryPoint(clerkAuthenticationEntryPoint)
         );
         return http.build();
     }
